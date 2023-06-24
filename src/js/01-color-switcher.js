3953f87c075e1c67075c8047e1be083a48271ed1
Serhii Kondratenko <kondratenko.serhii3@gmail.com>
2023-06-24 23:07:19 +0300

    Init project

---
+++ src/js/01-color-switcher.js
@@ -0,0 +1,40 @@
+
+// const buttonStartColorsChangeEl = document.querySelector(".btn-start");
+// const buttonStopColorsChangeEl = document.querySelector(".btn-stop");
+
+// console.log(buttonStartColorsChangeEl);
+// console.log(buttonStopColorsChangeEl);
+
+// let timerId = null;
+
+// buttonStartColorsChangeEl.addEventListener("click", startColorChange);
+// buttonStopColorsChangeEl.addEventListener("click", stopColorChange)
+
+
+// function startColorChange() {
+//     console.log("Start");
+    
+//     timerId = setInterval(() => {
+//         document.body.style.backgroundColor = getRandomHexColor();
+//     }, 1000);
+//     buttonStartColorsChangeEl.disabled = true;
+// }
+
+
+// function stopColorChange() {
+//     console.log("Stop");
+//     clearInterval(timerId);
+//     buttonStartColorsChangeEl.disabled = false;
+// }
+
+
+// function getRandomHexColor() {
+//     return `#${Math.floor(Math.random() * 16777215)
+//       .toString(16)
+//       .padStart(6, 0)}`;
+//   }
+
+console.log("Start");
+    
+    
+
