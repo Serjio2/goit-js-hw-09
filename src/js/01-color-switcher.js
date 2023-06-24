8e9c5db65c7ffcb300caa48a82226a0769530328
Serhii Kondratenko <kondratenko.serhii3@gmail.com>
2023-06-24 23:32:44 +0300

    ++

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,40 +1,39 @@
 
-// const buttonStartColorsChangeEl = document.querySelector(".btn-start");
-// const buttonStopColorsChangeEl = document.querySelector(".btn-stop");
+const buttonStartColorsChangeEl = document.querySelector(".btn-start");
+const buttonStopColorsChangeEl = document.querySelector(".btn-stop");
 
 // console.log(buttonStartColorsChangeEl);
 // console.log(buttonStopColorsChangeEl);
 
-// let timerId = null;
+let timerId = null;
 
-// buttonStartColorsChangeEl.addEventListener("click", startColorChange);
-// buttonStopColorsChangeEl.addEventListener("click", stopColorChange)
+buttonStartColorsChangeEl.addEventListener("click", startColorChange);
+buttonStopColorsChangeEl.addEventListener("click", stopColorChange)
 
 
-// function startColorChange() {
-//     console.log("Start");
+function startColorChange() {
+    // console.log("Start");
     
-//     timerId = setInterval(() => {
-//         document.body.style.backgroundColor = getRandomHexColor();
-//     }, 1000);
-//     buttonStartColorsChangeEl.disabled = true;
-// }
+    timerId = setInterval(() => {
+        document.body.style.backgroundColor = getRandomHexColor();
+    }, 1000);
+    buttonStartColorsChangeEl.disabled = true;
+}
 
 
-// function stopColorChange() {
-//     console.log("Stop");
-//     clearInterval(timerId);
-//     buttonStartColorsChangeEl.disabled = false;
-// }
+function stopColorChange() {
+    // console.log("Stop");
+    clearInterval(timerId);
+    buttonStartColorsChangeEl.disabled = false;
+}
 
 
-// function getRandomHexColor() {
-//     return `#${Math.floor(Math.random() * 16777215)
-//       .toString(16)
-//       .padStart(6, 0)}`;
-//   }
+function getRandomHexColor() {
+    return `#${Math.floor(Math.random() * 16777215)
+      .toString(16)
+      .padStart(6, 0)}`;
+  }
 
-console.log("Start");
     
     
 
