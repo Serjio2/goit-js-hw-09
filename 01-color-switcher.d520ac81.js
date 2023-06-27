592e98451957a0efe652d21b20b791c2715f540e
Serjio2 <130094175+Serjio2@users.noreply.github.com>
2023-06-27 10:31:32 +0000

    Deploying to gh-pages from @ Serjio2/goit-js-hw-09@22bf1c9cb3b7d9cb5515e4058e23f022454746d7 🚀

---
+++ 01-color-switcher.d520ac81.js
@@ -0,0 +1,2 @@
+!function(){var t=document.querySelector(".btn-start"),e=document.querySelector(".btn-stop"),n=null;t.addEventListener("click",(function(){n=setInterval((function(){document.body.style.backgroundColor="#".concat(Math.floor(16777215*Math.random()).toString(16).padStart(6,0))}),1e3),t.disabled=!0,e.disabled=!1})),e.addEventListener("click",(function(){clearInterval(n),t.disabled=!1,e.disabled=!0}))}();
+//# sourceMappingURL=01-color-switcher.d520ac81.js.map
